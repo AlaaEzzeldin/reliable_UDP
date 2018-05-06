fb86dc24583247c99702aedd5dd4916a8e91fe67
Alaa <ala.ezzeldin@gmail.com>
2018-05-06 10:29:05 +0200

    adding checksum

--- FSM/Client_states.py
+++ FSM/Client_states.py
@@ -6,20 +6,29 @@ import utility
 class Waiting_for_0_From_below(State):
 
     def on_event(self, event):
-        # if event == 0:
         try:
             sock = event
             received_packet, server_address = sock.recvfrom(512)
             check_sum, length, seq_number, data = utility.extract_data(received_packet)  # extract the data
-            received_file = open(client_received_file, "ab")
-            received_file.write(data)  # deliver the data to the upper layer
-            received_file.close()
-            ack_packet = utility.make_ack_packet(0, 0)  # create ACK packet
-            sock.sendto(ack_packet, server_address)  # sen ACK packet to the server
-            if utility.expected_seqNumber(0, seq_number):
+
+            # if correct & expected sequence number
+            if (check_sum == utility.get_checksum_client(str(data)) ^ 0xffff) & utility.expected_seqNumber(0, seq_number):
+
+                # deliver the data to the upper layer
+                received_file = open(client_received_file, "ab")
+                received_file.write(data)
+                print(data)
+                received_file.close()
+
+                # send ACK packet to the server
+                ack_packet = utility.make_ack_packet(0, 0)
+                sock.sendto(ack_packet, server_address)
                 return Waiting_for_1_From_below()
+
             else:
-                ack_packet = utility.make_ack_packet(0, 1)  # create ACK packe
+                print("retransimission")
+                print("here to put ")
+                ack_packet = utility.make_ack_packet(0, 1)  # create ACK packet
                 sock.sendto(ack_packet, server_address)  # send ACK packet to the server
         except :
             print("delay in receiving")
@@ -29,20 +38,31 @@ class Waiting_for_0_From_below(State):
 class Waiting_for_1_From_below(State):
 
     def on_event(self, event):
-        # if event == 1:
         try:
             sock = event
             received_packet, server_address = sock.recvfrom(512)
             check_sum, length, seq_number, data = utility.extract_data(received_packet)  # extract the data
-            received_file = open(client_received_file, "ab")
-            received_file.write(data)  # deliver the data to the upper layer
-            received_file.close()
-            ack_packet = utility.make_ack_packet(0, 1)  # create ACK packet
-            sock.sendto(ack_packet, server_address)  # sen ACK packet to the server
-            if utility.expected_seqNumber(1, seq_number):
+            print(len(data))
+            print(check_sum)
+            print(utility.get_checksum_client(str(data)) ^ 0xffff)
+
+            # if correct & expected sequence number
+            if (check_sum == utility.get_checksum_client(str(data))):
+                # deliver the data to the upper layer
+                received_file = open(client_received_file, "ab")
+                received_file.write(data)
+                print(data)
+                received_file.close()
+
+                # send ACK packet to the server
+                ack_packet = utility.make_ack_packet(0, 1)
+                sock.sendto(ack_packet, server_address)
                 return Waiting_for_0_From_below()
+
             else:
-                ack_packet = utility.make_ack_packet(0, 0)  # create ACK packe
+                print("retransimission")
+                print("here to put ", (check_sum == utility.get_checksum_client(str(data)) ^ 0xffff))
+                ack_packet = utility.make_ack_packet(0, 0)  # create ACK packet
                 sock.sendto(ack_packet, server_address)  # send ACK packet to the server
         except:
             print("delay in receiving")
